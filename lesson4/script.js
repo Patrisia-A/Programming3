71000e8dbd17611796af48d95dd6ff1fa1d4e869
user <patrisia.avanesyan.s@tumo.org>
2019-10-13 16:50:13 +0400

    Lesson 4

---
+++ lesson4/script.js
@@ -0,0 +1,33 @@
+// function windowLoad() {
+//     console.log("Window is loaded");
+//  }
+//  window.onload = windowLoad;
+//  // .addEventListener("onload",windowLoad)
+ 
+
+
+//  function preload(){
+//      console.log("Window is loaded")
+//  }
+
+// //  setup(), draw()...
+
+
+// function click(evt){
+//     console.log(evt.pageX,evt.pageY);
+// }
+// window.onclick = click;
+
+
+// function mousePressed(){
+//     console.log(mouseX,mouseY)
+// }
+
+
+
+// function keydown(evt){
+//     console.log("You printed " + evt.key);
+// }
+// window.onkeydown = keydown;
+
+
